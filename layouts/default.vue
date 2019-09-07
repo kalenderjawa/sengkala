56b2653336d50b8289cf075b6133035cb4ede2ce
Equan Pr <equan.p@gmail.com>
2019-09-07 18:33:36 +0700

    cleanup

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,23 +3,3 @@
     <nuxt />
   </section>
 </template>
-
-<style>
-.level {
-  margin: 3.4em;
-}
-
-.level-item p {
-  letter-spacing: 0.07em;
-}
-
-.level-item p#sasi {
-  font-weight: 600;
-}
-
-.level-item p#kj {
-  text-transform: uppercase;
-  font-weight: 100;
-}
-
-</style>
