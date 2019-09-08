47da0a5e632498ad9d31eb693267b0e5b40cb7ca
Equan Pr <equan.p@gmail.com>
2019-09-08 19:33:12 +0700

    Cleanup

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,12 @@
 <template>
   <section class="section">
+    <!--
     <Navi/>
+    -->
     <nuxt/>
   </section>
 </template>
 
 <script>
-import Navi from '~/components/Navi.vue'
-
-export default {
-  components: {
-    Navi
-  }
-}
+export default {}
 </script>  
