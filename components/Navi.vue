365083593016ee2dc12855e969c2d075be301bcb
Equan Pr <equan.p@gmail.com>
2019-09-08 11:42:01 +0700

    Komponen navigasi

---
+++ components/Navi.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <nav class="navbar is-fixed-bottom">
+     
+
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
