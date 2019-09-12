af0a22f5a6aee660b414ea730a794b2d3d67103d
Equan Pr <equan.p@gmail.com>
2019-09-12 18:38:29 +0700

    Mengatasi tumpuk text version app di screen nexus 5

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,7 +6,7 @@
     <div class="section app-info">
       <div class="level is-mobile">
       <div class="level-left">
-        <p class="app"><span>Sengkala</span> (beta)</p>
+        <p class="app"><span class="app-logo">Sengkala</span> <span class="app-version">(beta)</span></p>
       </div>
       <div class="level-right">
         <p class="tag"><a href="https://kalenderjawa.dev">kalenderjawa.dev</a></p>
