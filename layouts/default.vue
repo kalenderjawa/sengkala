683f83bf9e96184d10f94fde84e3d590b6c29e0e
Equan Pr <equan.p@gmail.com>
2019-09-12 12:49:21 +0700

    App title and author

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,18 @@
     <!--
     <Navi/>
     -->
-    <nuxt/>
+    <div class="section app-info">
+      <div class="level is-mobile">
+      <div class="level-left">
+        <p class="tag"><span>tgl</span> (beta)</p>
+      </div>
+      <div class="level-right">
+        <p class="tag"><a href="https://kalenderjawa.dev">kalenderjawa.dev</a></p>
+      </div>
+    </div>
+    </div>
+    
+    <nuxt />
   </div>
 </template>
 
